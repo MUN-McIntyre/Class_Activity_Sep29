9074bde914694c3f8c2dd985d203b6736a638943
aabirbasu <97454850+aabirbasu@users.noreply.github.com>
2023-09-29 10:49:54 -0230

    Update notes.py

--- notes.py
+++ notes.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 image = cv2.imread("notes.png", cv2.IMREAD_GRAYSCALE)
 height, width = image.shape[0], image.shape[1]
 grey = list()
-for i in range(200, 250):
+for i in range(140, 255):
   grey.append(i)
 for x in range (width):
   for y in range (height):
