3d3c39f124126c7a12e38f5a66f822c4790fa6cc
aabirbasu <97454850+aabirbasu@users.noreply.github.com>
2023-09-29 10:26:55 -0230

    Update notes.py

--- notes.py
+++ notes.py
@@ -5,9 +5,15 @@ import numpy as np
 
 image = cv2.imread("notes.png", cv2.IMREAD_GRAYSCALE)
 height, width = image.shape[0], image.shape[1]
+white = list()
+for i in range(230, 255):
+  white.append(i)
+black = list()
+for i in range(-1, 20):
+  black.append(i)  
 for x in range (width):
   for y in range (height):
-    if image [y][x] != 0 or image [y][x] != 255:
+    if image [y][x] not in white and image [y][x] not in black:
       image [y][x] = 255
     
 new_image = cv2.imwrite("new_notes.png", image)
