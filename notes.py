9c5794d1bb3702477d44f6a9932024ced5f20549
aabirbasu <97454850+aabirbasu@users.noreply.github.com>
2023-09-29 10:38:05 -0230

    Update notes.py

--- notes.py
+++ notes.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import math
 import numpy as np
+from matplotlib import pylot as plt
 
 image = cv2.imread("notes.png", cv2.IMREAD_GRAYSCALE)
 height, width = image.shape[0], image.shape[1]
@@ -16,4 +17,7 @@ for x in range (width):
     if image [y][x] not in white and image [y][x] not in black:
       image [y][x] = 255
     
+plt.hist(image.ravel(),256,[0,256])
+plt.show()
+
 new_image = cv2.imwrite("new_notes.png", image)
