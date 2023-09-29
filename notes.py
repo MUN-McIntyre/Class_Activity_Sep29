4adc723a486eba31dbc5ce13fafd1b4ac8550f29
aabirbasu <97454850+aabirbasu@users.noreply.github.com>
2023-09-29 10:41:18 -0230

    Update notes.py

--- notes.py
+++ notes.py
@@ -6,15 +6,12 @@ from matplotlib import pyplot as plt
 
 image = cv2.imread("notes.png", cv2.IMREAD_GRAYSCALE)
 height, width = image.shape[0], image.shape[1]
-white = list()
-for i in range(205, 255):
-  white.append(i)
-black = list()
-for i in range(-1, 40):
-  black.append(i)  
+grey = list()
+for i in range(200, 250):
+  grey.append(i)
 for x in range (width):
   for y in range (height):
-    if image [y][x] not in white and image [y][x] not in black:
+    if image [y][x] in grey:
       image [y][x] = 255
     
 plt.hist(image.ravel(),256,[0,256])
